deabe47e92f96311da1a39c1b92fd52a0ba1d819
Dan <danretegan@yahoo.com>
2024-07-20 15:25:38 +0300

    update Vercel

--- client/src/redux/auth/operations.js
+++ client/src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://healthapp-server.vercel.app/";
+axios.defaults.baseURL = 'https://taskpro-server-mu.vercel.app';
 
 const utils = {
   setAuthHeader: (token) =>
