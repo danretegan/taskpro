6796e5252caa5260911c4421c1f1c5366a4b301a
RaresDrg <dragan_rares13@yahoo.com>
2024-07-24 13:00:44 +0300

    pr fix
    pr fix

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import logoImage from '../../assets/images/Logo.png';
+import logo from '../../assets/images/Logo.png';
 import icons from '../../assets/icons/icons.svg';
 import Container from '../../components/common/Container/Container.styled';
 import { ButtonOnHomePage } from '../../components/common/FormButton/FormButton.styled';
@@ -12,7 +12,7 @@ const HomePage = ({ className: styles }) => {
       <Container>
         <div className="logo">
           <a href="/" className="logo">
-            <img src={logoImage} alt="computer" />
+            <img src={logo} alt="computer" />
             <div className="logo-text">
               <svg>
                 <use href={`${icons}#icon-logoBlackWhite`}></use>
