a9fcfdd6e913fe1810bfa4a51d920ce4bb92170e
Dan <danretegan@yahoo.com>
2024-07-27 08:35:16 +0300

    Update TaskProLogo.jsx
    navigate('/dashboard');

--- client/src/components/LeftSideBar/TaskProLogo/TaskProLogo.jsx
+++ client/src/components/LeftSideBar/TaskProLogo/TaskProLogo.jsx
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import icons from '../../../assets/icons/icons.svg';
 
 const TaskProLogo = ({ className: styles }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/dashboard');
+  };
+
   return (
-    <div className={styles}>
+    <div className={styles} onClick={handleClick}>
       <svg>
         <use href={`${icons}#icon-logoBlue`}></use>
       </svg>
