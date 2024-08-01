7d106c6dfccb4712a12fca4fba2f39167dbf1a3b
Dan <danretegan@yahoo.com>
2024-08-01 19:22:56 +0300

    Confirm Delete (modal)

--- client/src/components/LeftSideBar/ProjectListItem/ProjectListItem.jsx
+++ client/src/components/LeftSideBar/ProjectListItem/ProjectListItem.jsx
@@ -6,11 +6,25 @@ import {
   StyledProjectListItem,
 } from './ProjectListItem.styled';
 import icons from '../../../assets/icons/icons.svg';
+import ConfirmDeleteModal from '../ConfirmDeleteModal/ConfirmDeleteModal';
+import { useState } from 'react';
 
 const ProjectListItem = ({ project, onEdit, onDelete }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+
+  const handleDelete = () => {
+    onDelete(project._id);
+    handleCloseModal();
+  };
+
   return (
     <StyledProjectListItem>
-      <ProjectSectionButton onClick={() => console.log('Project button clicked')}>
+      <ProjectSectionButton
+        onClick={() => console.log('Project button clicked')}
+      >
         <ProjectIcon>
           <svg>
             <use href={`${icons}#${project.icon}`}></use>
@@ -24,12 +38,20 @@ const ProjectListItem = ({ project, onEdit, onDelete }) => {
             <use href={`${icons}#icon-pencil`}></use>
           </svg>
         </EditIconButton>
-        <EditIconButton onClick={onDelete}>
+        <EditIconButton onClick={handleOpenModal}>
           <svg>
             <use href={`${icons}#icon-trash`}></use>
           </svg>
         </EditIconButton>
       </IconsSection>
+      {isModalOpen && (
+        <ConfirmDeleteModal
+          isOpen={isModalOpen}
+          onClose={handleCloseModal}
+          onConfirm={handleDelete}
+          projectName={project.title}
+        />
+      )}
     </StyledProjectListItem>
   );
 };
