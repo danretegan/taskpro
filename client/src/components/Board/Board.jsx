4feeaba7eddc5462f8f05ebf45a2d108407274ce
Marius Birsan <marius.birsan90@outlook.com>
2024-08-01 09:22:46 +0300

    Board route created

---
+++ client/src/components/Board/Board.jsx
@@ -0,0 +1,75 @@
+import {
+  BoardContainer,
+  AddCardButton,
+  BoardHeader,
+  BoardTitle,
+  Title,
+  Filters,
+  Text,
+  FilterIcon,
+  BurgerMenu,
+  Theme,
+  UserName,
+  UserPicture,
+  Nav,
+} from './Board.styled';
+import StyledColumnContainer from '../ColumnContainer/ColumnContainer.styled';
+import StyledCardContent from '../CardContent/CardContent.styled';
+import sprite from '../../assets/icons/icons.svg';
+import image from '../../assets/images/vector.png';
+
+const Board = ({ columns }) => {
+  return (
+    <>
+      <BoardHeader>
+        <BurgerMenu>
+          <svg width="18" height="18">
+            <use href={`${sprite}#icon-burger-menu`}></use>
+          </svg>
+        </BurgerMenu>
+        <Nav>
+          <Theme>
+            Theme
+            <svg width="18" height="18">
+              <use href={`${sprite}#icon-dropDown`}></use>
+            </svg>
+          </Theme>
+          <UserName>Ivetta</UserName>
+          <UserPicture>
+            <img src={image} alt="User image"></img>
+          </UserPicture>
+        </Nav>
+      </BoardHeader>
+      <BoardTitle>
+        <Title>Board Title</Title>
+        <Filters>
+          <FilterIcon>
+            <svg width="18" height="18">
+              <use href={`${sprite}#icon-filter`}></use>
+            </svg>
+          </FilterIcon>
+          <Text>Filters</Text>
+        </Filters>
+      </BoardTitle>
+      <BoardContainer>
+        {columns.map((column, index) => (
+          <StyledColumnContainer key={index}>
+            <h2>{column.title}</h2>
+            {column.cards.map((card, cardIndex) => (
+              <StyledCardContent
+                key={cardIndex}
+                title={card.title}
+                description={card.description}
+                priority={card.priority}
+                deadline={card.deadline}
+              />
+            ))}
+            <AddCardButton>+ Add another card</AddCardButton>
+          </StyledColumnContainer>
+        ))}
+      </BoardContainer>
+    </>
+  );
+};
+
+export default Board;
