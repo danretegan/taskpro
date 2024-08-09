53bb2fdc57d6844992251057d18b3b3906d0cd22
Dan <danretegan@yahoo.com>
2024-08-09 12:11:25 +0300

    Update Dashboard.jsx

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,40 +1,11 @@
-import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import useResponsive from '../../hooks/useResponsive';
 import { DashboardContainer, StyledParagraph } from './Dashboard.styled';
-import { loadImage } from '../../assets/images/loadImage';
 
 const Dashboard = () => {
   const selectedProject = useSelector(state => state.projects.selectedProject);
-  const { isOnMobile, isOnTablet, isOnDesktop } = useResponsive();
-  const [backgroundPath, setBackgroundPath] = useState(null);
-
-  let screenSize;
-  if (isOnDesktop) screenSize = 'desktop';
-  else if (isOnTablet) screenSize = 'tablet';
-  else if (isOnMobile) screenSize = 'mobile';
-
-  const isRetina = window.devicePixelRatio > 1;
-
-  useEffect(() => {
-    const fetchBackground = async () => {
-      if (selectedProject?.background) {
-        const fileName = selectedProject.background
-          .split('/')
-          .pop()
-          .replace('.png', '');
-        const path = await loadImage(fileName, screenSize, isRetina);
-        setBackgroundPath(path);
-      }
-    };
-    fetchBackground();
-  }, [selectedProject, screenSize, isRetina]);
 
   return (
-    <DashboardContainer
-      $selectedProject={selectedProject}
-      $background={backgroundPath}
-    >
+    <DashboardContainer $selectedProject={selectedProject}>
       {selectedProject ? (
         <>
           <StyledParagraph $selectedProject={selectedProject}>
